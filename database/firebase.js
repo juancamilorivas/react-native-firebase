936cfe826cd6dac2185ef6579c880efacbb65899
Camilo Rivas <67404147+juancamilorivas@users.noreply.github.com>
2023-06-07 11:33:43 -0500

    eliminacion de secret

--- database/firebase.js
+++ database/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCelmTBObuenEOUBumm0ZC3zLDPSmatVoY",
-  authDomain: "react-native-firebase-e7342.firebaseapp.com",
-  projectId: "react-native-firebase-e7342",
-  storageBucket: "react-native-firebase-e7342.appspot.com",
-  messagingSenderId: "428193734114",
-  appId: "1:428193734114:web:b28c88e7f30cc1e6a82dbb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
